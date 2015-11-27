f5aa0ebc2fc784b893a666e5de17614e00890fca
Maciej Szlosarczyk <sosek@sosek.net>
2015-11-27 10:02:58 +0200

    Various fixes to the grid. Added academic reference and changed the expanded text to be substantial.

--- js/data.js
+++ js/data.js
@@ -8,13 +8,16 @@ var interactions = [
         name: "MobiES",
         summary: "MobiES allows users to interact with data contained in a mobile device on a larger screen by detecting the proximity between the two and expanding the mobile's interface into the screen. More devices can be connected so you can share information between them.",
         image: "Diagram_for_MobiES.png",
-        expanded_data: "Something that is displayed when is expanded."
+        expanded_data: "MobiES facilitates extending the display of a mobile device to a public screen and using both displays to form a larger screen area. Bringing a phone next to the public screen triggers a special user interface to be shown, which allows the user to browse content or share content between 2 mobile devices positioned on either side of the public display and dragging content from one phone to another.",
+        reference: "Schneider, D., Seifert, J., & Rukzio, E. (2012). MobiES: extending mobile interfaces using external screens. In Proceedings of the 11th International Conference on Mobile and Ubiquitous Multimedia (p. 59). ACM."
     },
     {
         id: 2,
         name: "The Conduit",
         summary: "The conduit uses your body as a data-transferring metaphor - with your non-dominant hand, tap the receiving device; then with your dominant hand, tap the piece of data to transfer - the transfer will go \"through your body\" from origin to destination.",
-        image: "Conduit-example.png"
+        image: "Conduit-example.png",
+        expanded_data: "Chen et al. developed a Conduit technique to facilitate information transfer among e-readers in their United Slates system. In this technique the non-dominant hand of the user designates the target device and the dominant hand select the item to be transferred. In this case the user\'s body acts an imaginary conduit between the 2 devices, facilitating the file transfer. This technique can also be used to navigate to pages corresponding to a thumbnail selected on another device. In this case one device is used for displaying thumbnail previews and the other one for reading the corresponding pages.",
+        reference: "Chen, N., Guimbretiere, F., & Sellen, A. (2012). Designing a multi-slate reading environment to support active reading activities. ACM Transactions on Computer-Human Interaction (TOCHI), 19(3), 18."
     },
     {
         id: 3,
